86496db9df5053b446077f377d2028620ca3b40d
= <edomeciar@gmail.com>
2015-03-26 14:11:51 +0100

    Add name and role into the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,12 @@
-$("#main").append("Eduard Meciar");
+var formattedName;
+var formattedRole;
+
+
+var role ="Front End Web Developer"
+
+formattedName = HTMLheaderName.replace("%data%","Eduard Meciar");
+formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
