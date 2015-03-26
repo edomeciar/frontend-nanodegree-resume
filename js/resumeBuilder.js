a4498d1745c926541b254b125288c29ad146019e
= <edomeciar@gmail.com>
2015-03-26 16:36:59 +0100

    Add education, project, work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,147 @@ var formattedName;
 var formattedRole;
 
 
-var role ="Front End Web Developer"
+var bio = {
+	"name" : "Eduard Meciar",
+	"role" : "Front End Web Developer",
+	"contact": {
+		"mobile": "+421 911 624 266",
+		"email": "edomeciar@gmail.com",
+		"github": "edomeciar",
+		"location": "Bratislava SVK"
+	},
+	"bioPic" : "https://media.licdn.com/mpr/mpr/shrink_500_500/p/3/000/10b/0c9/373adec.jpg",
+	"welcomeMessage" : "Ola Amigos!",
+	"skils" : ["Programing","JS","SQL","HTML","C#","And more..."],
+	"education" : {
+		"Schools"  : [
+			{
+				"name" : "Univesity of Zilina",
+				"degree" : "Bc.",
+				"dates" : "2007 - 2010",
+				"location" : "Zilina",
+				"major" : "Informatics"
+			},
+			{
+				"name" : "Univesity of Zilina",
+				"degree" : "Ing",
+				"dates" : "2010 - 2013",
+				"location" : "Zilina",
+				"major" : "Applied Informatics"
+			}
+		],
+		"onlineCourses" : [
+			{
+				"title" : "Git",
+				"school" : "Udacity",
+				"dates" : "2015",
+				"url" : "https://www.udacity.com/course/ud775"
+			}
+		]
+	},
+	"work" : {
+		"jobs" :[
+				{
+					"employer" : "Accenture / Avanade",
+					"title" : "Programer",
+					"dates" : "2012 - 2013",
+					"location" : "Bratislava",
+					"description" : "Dynamics CRM Programer"
+				},
+				{
+					"employer" : "AT&T Slovakia",
+					"title" : "Programer / Softweare Architect",
+					"dates" : "2013 - Today",
+					"location" : "Bratislava",
+					"description" : "Front End / Back End Developer / Software Architect"
+				}
+			]
+	},
+	"projects" : [
+		{
+			"title" : "SSR project",
+			"date" : "2014",
+			"description" : "Self Service Reporting intranet",
+			"images" : ["",""]
+		},
+	]
+};
 
-formattedName = HTMLheaderName.replace("%data%","Eduard Meciar");
-formattedRole = HTMLheaderRole.replace("%data%",role);
+formattedName = HTMLheaderName.replace("%data%",bio.name);
+formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+formattedMobile = HTMLmobile.replace("%data%",bio.contact.mobile);
+formattedEmail = HTMLemail.replace("%data%",bio.contact.email);
+formattedGithub = HTMLgithub.replace("%data%",bio.contact.github);
+formattedLocation = HTMLlocation.replace("%data%",bio.contact.location);
+formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPic);
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+
+$("#topContacts").append(formattedLocation);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedMobile);
+$("#header").append(HTMLskillsStart);
+for (i = 0; i < bio.skils.length; i++) {
+    $("#skills").append(HTMLskills.replace("%data%",bio.skils[i]));
+}
+$("#header").prepend(formattedWelcomeMsg);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedBioPic);
+
+for (i = 0; i < bio.education.Schools.length; i++) {
+	addEducationSchool(bio.education.Schools[i]);
+}
+$("#education").append(HTMLonlineClasses);
+for (i = 0; i < bio.education.onlineCourses.length; i++) {
+	addOnlineCourse(bio.education.onlineCourses[i]);
+}
+
+for (i = 0; i < bio.work.jobs.length; i++) {
+	addWork(bio.work.jobs[i]);
+}
+
+for (i = 0; i < bio.projects.length; i++) {
+	addProject(bio.projects[i]);
+}
+
+function addProject (_project){
+	$("#projects").append(HTMLprojectStart);
+	$("#projects div.project-entry:last").append(HTMLprojectTitle.replace("%data%",_project["title"]));
+	$("#projects div.project-entry:last").append(HTMLprojectDates.replace("%data%",_project["date"]));
+	$("#projects div.project-entry:last").append(HTMLprojectDescription.replace("%data%",_project["description"]));
+	for (var j = 0; j < _project.images.length; j++) {
+		$("#projects div.project-entry:last").append(HTMLprojectImage.replace("%data%",_project.images[j]));
+	};
+
+};
+
+function addOnlineCourse (_onlineCourse){
+	$("#education").append(HTMLschoolStart);
+	$("#education div.education-entry:last").append(HTMLonlineTitle.replace("%data%",_onlineCourse["title"]));
+	$("#education div.education-entry:last").append(HTMLonlineSchool.replace("%data%",_onlineCourse["school"]));
+	$("#education div.education-entry:last").append(HTMLonlineDates.replace("%data%",_onlineCourse["dates"]));
+	$("#education div.education-entry:last").append(HTMLonlineURL.replace("%data%",_onlineCourse["url"]));
+};
+
+function addEducationSchool(_shoolInfo){
+	$("#education").append(HTMLschoolStart);
+	$("#education div.education-entry:last").append(HTMLschoolName.replace("%data%",_shoolInfo["name"]));
+	$("#education div.education-entry:last").append(HTMLschoolDegree.replace("%data%",_shoolInfo["degree"]));
+	$("#education div.education-entry:last").append(HTMLschoolDates.replace("%data%",_shoolInfo["dates"]));
+	$("#education div.education-entry:last").append(HTMLschoolLocation.replace("%data%",_shoolInfo["location"]));
+	$("#education div.education-entry:last").append(HTMLschoolMajor.replace("%data%",_shoolInfo["major"]));
+};
+
+function addWork(_workInfo){
+	var HTMLworkStart = '<div class="work-entry"></div>';
+
+	$("#workExperience").append(HTMLworkStart);
+	$("#workExperience div.work-entry:last").append(HTMLworkEmployer.replace("%data%",_workInfo.employer));
+	$("#workExperience div.work-entry:last").append(HTMLworkTitle.replace("%data%",_workInfo.title));
+	$("#workExperience div.work-entry:last").append(HTMLworkDates.replace("%data%",_workInfo.dates));
+	$("#workExperience div.work-entry:last").append(HTMLworkLocation.replace("%data%",_workInfo.location));
+	$("#workExperience div.work-entry:last").append(HTMLworkDescription.replace("%data%",_workInfo.description));
+};
 
